205381249609647451d13c1af3c00a2a97f4030e
sheric98 <eric.r.shang@gmail.com>
2020-03-20 14:11:56 -0500

    fixing null users

--- index.js
+++ index.js
@@ -12,22 +12,35 @@ app.get('/', function(req, res) {
 var users = new Object();
 
 io.sockets.on('connection', function(socket) {
+    function validSocket() {
+        return (socket.hasOwnProperty('username') && (typeof socket.username !== "undefined")
+            && socket.username != null);
+    }
     socket.emit('display', users);
     socket.on('username', function(username) {
-        socket.username = username;
-        users[socket.id] = socket.username;
-        io.emit('is_online', '<i>' + socket.username + ' has a big butt...</i>');
-        io.emit('new_user', socket.username, socket.id);
+        if ((typeof username !== "undefined") && (username != null)) {
+            socket.username = username;
+            users[socket.id] = socket.username;
+            io.emit('is_online', '<i>' + socket.username + ' has a big butt...</i>');
+            io.emit('new_user', socket.username, socket.id);
+        }
     });
 
     socket.on('disconnect', function(disconnect) {
-        delete users[socket.id];
-        io.emit('is_online', '<i>' + socket.username + ' has a small butt...</i>');
-        io.emit('remove_user', socket.id);
+        if (validSocket()) {
+            delete users[socket.id];
+            io.emit('is_online', '<i>' + socket.username + ' has a small butt...</i>');
+            io.emit('remove_user', socket.id);
+        }
     });
 
     socket.on('chat_message', function(message) {
-        io.emit('chat_message', '<b>' + socket.username + '</b>: ' + message);
+        if (!validSocket()) {
+            socket.emit('no_name', '<b><i>Please refresh and enter a name to chat.</i></b>');
+        }
+        else{
+            io.emit('chat_message', '<b>' + socket.username + '</b>: ' + message);
+        }
     });
 });
 
