c3502cea6f58a41362c07469f60dcd01bf659139
sheric98 <eric.r.shang@gmail.com>
2020-03-20 12:10:20 -0500

    added online user list

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
+var users = new Object();
+
 io.sockets.on('connection', function(socket) {
+    socket.emit('display', users);
     socket.on('username', function(username) {
         socket.username = username;
+        users[socket.id] = socket.username;
         io.emit('is_online', '<i>' + socket.username + ' has a big butt...</i>');
+        io.emit('new_user', socket.username, socket.id);
     });
 
     socket.on('disconnect', function(disconnect) {
+        delete users[socket.id];
         io.emit('is_online', '<i>' + socket.username + ' has a small butt...</i>');
+        io.emit('remove_user', socket.id);
     });
 
     socket.on('chat_message', function(message) {
