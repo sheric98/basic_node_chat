55b9024cd9b1e670e096f63ffef3d927fa4e0005
sheric98 <eric.r.shang@gmail.com>
2020-03-21 02:34:38 -0500

    added private messaging

--- index.js
+++ index.js
@@ -10,17 +10,38 @@ app.get('/', function(req, res) {
 });
 
 var users = new Object();
+var names = new Object();
+
+function isDM(msg) {
+    if (msg[0] == '@') {
+        if (msg.indexOf(' ') > 1) {
+            var space = msg.indexOf(' ');
+            var target = msg.substring(1, space);
+            if (msg.length > space + 1) {
+                if (target in names) {
+                    var dm = msg.substring(space + 1, msg.length);
+                    return [true, target, dm];
+                }
+            }
+        }
+    }
+    return [false, null, null];
+}
 
 io.sockets.on('connection', function(socket) {
     function validSocket() {
         return (socket.hasOwnProperty('username') && (typeof socket.username !== "undefined")
             && socket.username != null);
     }
-    socket.emit('display', users);
+
+    socket.emit('new_conn', names);
+
     socket.on('username', function(username) {
         if ((typeof username !== "undefined") && (username != null)) {
             socket.username = username;
-            users[socket.id] = socket.username;
+            users[socket.id] = socket;
+            names[socket.username] = socket.id;
+            console.log('created username ' + username);
             io.emit('is_online', '<i>' + socket.username + ' has a big butt...</i>');
             io.emit('new_user', socket.username, socket.id);
         }
@@ -29,6 +50,8 @@ io.sockets.on('connection', function(socket) {
     socket.on('disconnect', function(disconnect) {
         if (validSocket()) {
             delete users[socket.id];
+            delete names[socket.username];
+            console.log('deleted ' + socket.username);
             io.emit('is_online', '<i>' + socket.username + ' has a small butt...</i>');
             io.emit('remove_user', socket.id);
         }
@@ -36,10 +59,20 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('chat_message', function(message) {
         if (!validSocket()) {
-            socket.emit('no_name', '<b><i>Please refresh and enter a name to chat.</i></b>');
+            socket.emit('no_name', '<b><i>Please refresh and enter a name to chat.</i></b>', names);
         }
         else{
-            io.emit('chat_message', '<b>' + socket.username + '</b>: ' + message);
+            var checkDM = isDM(message);
+            if (checkDM[0]) {
+                var target = checkDM[1];
+                var DM = checkDM[2];
+                var target_id = names[target];
+                users[target_id].emit('chat_message', '<b>' + socket.username + ' => you</b>: ' + DM);
+                socket.emit('chat_message', '<b>you => ' + socket.username + '</b>: ' + DM);
+            }
+            else {
+                io.emit('chat_message', '<b>' + socket.username + '</b>: ' + message);
+            }
         }
     });
 
